a7eff0016584144c46183a54c2bf4c2ec46bd6b0
yahir <yahirrip@gmail.com>
2026-04-20 00:13:40 -0600

    Add R script for sequence, mean, and sum calculations

---
+++ primerospasos/mediaysuma.r
@@ -0,0 +1,13 @@
+# Secuencia de 20 a 50
+seq_20_50 <- 20:50
+
+# Media de los números del 20 al 60
+media_20_60 <- mean(20:60)
+
+# Suma de los números del 51 al 91
+suma_51_91 <- sum(51:91)
+
+# Mostrar resultados
+print(seq_20_50)
+print(media_20_60)
+print(suma_51_91)
