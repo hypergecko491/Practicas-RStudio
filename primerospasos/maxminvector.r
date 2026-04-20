da785f2fd043228b6e55865e0f667c1489d76937
yahir <yahirrip@gmail.com>
2026-04-20 00:14:32 -0600

    Add R script to find min and max of vectors

---
+++ primerospasos/maxminvector.r
@@ -0,0 +1,11 @@
+vec_a <- c(10, 20, 30, 4, 50, -60)
+vec_b <- c(10, 20, 30, 4, 50, -60)
+
+min_a <- min(vec_a)
+max_a <- max(vec_a)
+
+min_b <- min(vec_b)
+max_b <- max(vec_b)
+
+print(c("Mínimo de vec_a" = min_a, "Máximo de vec_a" = max_a))
+print(c("Mínimo de vec_b" = min_b, "Máximo de vec_b" = max_b))
